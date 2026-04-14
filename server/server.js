76b1087ece1f6710c7c167d42a0ed364bd76a90f
Aditya Bhatia <adityabhatia161@gmail.com>
2026-04-14 15:27:34 +0530

    Fix: updated wildcard route for deployment

--- server/server.js
+++ server/server.js
@@ -99,7 +99,7 @@ app.listen(PORT, () => console.log(`Server active on port ${PORT}`));
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
